58638632bc043deee54378e30d4c6f6e4aa7dc25
Jones3106 <64327198+Jones3106@users.noreply.github.com>
2020-05-23 22:31:07 +0500

    Added German translation

---
+++ locales/de.js
@@ -0,0 +1,9 @@
+NodeRP.Locales["de"] = {
+	"welcome_msg": `Willkommen zu ^1${Config.ServerName}.`,
+	"firstspawnloc": "Du hast bei erzeugt ~g~Standardspeicherort~s~.",
+	"localchat": "sagt",
+	"shout": "Geschrei",
+	"no_admins_online": "Es sind keine Administratoren online.",
+	"admins_online": "Administratoren online",
+	"invalid_cmd": "^1Ungültig ^0cmd, ^0art ^2/cmds^0 einen Blick darauf werfen das befehle verfügbar."
+};
